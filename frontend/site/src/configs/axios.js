2d9ca23015568680285b0c596919169429fbb5da
claude tsiangana <claudetsiangana@gmail.com>
2026-06-03 17:48:44 +0200

    add gateway

--- frontend/site/src/configs/axios.js
+++ frontend/site/src/configs/axios.js
@@ -4,36 +4,43 @@ import axios from "axios";
 // const API_KEY = ""
 const Axios = axios.create({
   withCredentials: true,
-  baseURL: process.env.NODE_ENV === "development" ? 'http://localhost:9000/api/v1' : 'https://api.airneis.tsiang.fr/api/v1',
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:9000/api/v1"
+      : "http://api.airnes.tsiang.fr/api/v1",
   headers: {
-    'X-API-KEY':'@&3gtebnduie-ygb4567jhgjv'
-  }
+    "X-API-KEY": "@&3gtebnduie-ygb4567jhgjv",
+  },
 });
 
-
-Axios.interceptors.request.use(function (config) {
-  const  token =  localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, function (error) {
-  return Promise.reject(error);
-});
-
+Axios.interceptors.request.use(
+  function (config) {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
 
 Axios.interceptors.response.use(
   (response) => response,
   (error) => {
-    if ((error?.response?.data?.message === "invalidToken" && error?.response.status === 403) ||
-     (error?.response?.data?.message === "Unauthorized" && error?.response.status === 401)
+    if (
+      (error?.response?.data?.message === "invalidToken" &&
+        error?.response.status === 403) ||
+      (error?.response?.data?.message === "Unauthorized" &&
+        error?.response.status === 401)
     ) {
-      if(localStorage.getItem('token')) {
-        localStorage.removeItem('token');
+      if (localStorage.getItem("token")) {
+        localStorage.removeItem("token");
       }
     }
     return Promise.reject(error);
-  }
+  },
 );
 
 export default Axios;
